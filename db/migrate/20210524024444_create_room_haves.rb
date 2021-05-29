3b621fcc6be0efdc1c567486ebd54679e3bf1d2a
itiapotami <yanagisawasoichi@gmail.com>
2021-05-29 16:07:31 +0900

    delete room_have mdel

--- db/migrate/20210524024444_create_room_haves.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoomHaves < ActiveRecord::Migration[6.0]
-  def change
-    create_table :room_haves do |t|
-      t.references    :room,       foreign_key: true
-      t.references    :facility,   foreign_key: true
-      t.timestamps
-    end
-  end
-end
