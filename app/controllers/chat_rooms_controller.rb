27f0e78c05814746b3b0ef6cae9f81150aa0ecfc
itiapotami <yanagisawasoichi@gmail.com>
2021-08-18 16:50:48 +0900

    chat_roomsコントローラーの変更

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,15 +1,15 @@
 class ChatRoomsController < ApplicationController
   
   def create
-    chat_room = ChatRoom.where(hotel_id: params[:hotel_id])
+    chat_room = ChatRoom.find_by(hotel_id: params[:hotel_id])
     if chat_room.blank?
       chat_room = ChatRoom.create(hotel_id: params[:hotel_id])
-      redirect_to action: :show, id: chat_room.id
-    else
-      redirect_to action: :show, id: chat_room.id
     end
+    redirect_to chat_room_path(id: chat_room.id)
   end
   def show
-    
+    @chat_room = ChatRoom.find(params[:id])
+    @chat_messages = ChatMessage.where(chat_room: @chat_room.id)
+    @chat_message = ChatMessage.new
   end
 end
