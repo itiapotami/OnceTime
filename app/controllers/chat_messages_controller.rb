6570ffe26b4baca3bbd427fbd231a1231b372fc5
itiapotami <yanagisawasoichi@gmail.com>
2021-08-18 16:49:23 +0900

    chat_messageコントローラーの作成

---
+++ app/controllers/chat_messages_controller.rb
@@ -0,0 +1,14 @@
+class ChatMessagesController < ApplicationController
+  
+  def create
+    @chat_message = ChatMessage.new(chat_message_params)
+    if @chat_message.save
+      redirect_to chat_room_path(id: @chat_message.chat_room_id)
+    end
+  end
+
+  private
+  def chat_message_params
+    params.require(:chat_message).permit(:content).merge(user_id: current_user.id, chat_room_id: params[:chat_room_id])
+  end
+end
