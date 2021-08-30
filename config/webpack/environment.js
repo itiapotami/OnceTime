a760745a8e57aa8301eadb2ca95890067d0331b5
itiapotami <yanagisawasoichi@gmail.com>
2021-08-30 15:00:36 +0900

    bootstrap @popperjsの追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,13 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
 
-environment.plugins.prepend('Provide',
+module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.append(
+  'Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
   })
 )
-module.exports = environment
